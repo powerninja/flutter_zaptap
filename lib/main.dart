0ffc85adbd53ba039f0608bec7bb2ace99b42166
powerninja <asdyuy23@gmail.com>
2024-04-28 01:40:38 +0900

    フォント設定

--- lib/main.dart
+++ lib/main.dart
@@ -42,10 +42,19 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
-        centerTitle: false,
-        title: Text(
-          widget.title,
-        ),
+        centerTitle: true,
+        title: Text(widget.title,
+            style: const TextStyle(fontWeight: FontWeight.bold)),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => {},
+          ),
+          IconButton(
+            icon: Icon(Icons.remove),
+            onPressed: () => {},
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -68,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.edit),
       ),
     );
   }
