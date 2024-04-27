43162684cc6aba5ebc717f92e7674b73dd2e2cf9
powerninja <asdyuy23@gmail.com>
2024-04-27 18:57:31 +0900

    appbarを背景色と同じにする

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.outline,
+        backgroundColor: Theme.of(context).colorScheme.background,
         centerTitle: false,
         title: Text(widget.title),
       ),
