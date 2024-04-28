1d687824a71a3fee64052e59bf4ad141ed987540
powerninja <asdyuy23@gmail.com>
2024-04-28 20:44:15 +0900

    不要な処理を削除

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.background,
         centerTitle: true,
         title: Text(widget.title,
-            style: const TextStyle(fontWeight: FontWeight.bold)),
+            style: const TextStyle(fontWeight: FontWeight.w700)),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
@@ -64,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              'test',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             ElevatedButton(
@@ -75,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => {},
         tooltip: 'Increment',
         child: const Icon(Icons.edit),
       ),
