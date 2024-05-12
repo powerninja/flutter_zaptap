77cd74ab20d155b827b8249956d94352e8cbb849
powerninja <asdyuy23@gmail.com>
2024-05-12 18:45:00 +0900

    メインファイルのimport文を修正 メモの保存日時をISO 8601形式に変更 テーブルのスキーマを更新する処理を追加 メモ一覧の表示を修正 メモIdをuuidに変更 pubspec.lockにcrypto、fixnum、sprintf、typed_data、uuidの依存関係を追加 Noteクラスのidプロパティをint型からString型に変更 メモの保存と取得機能を追加 Noteクラスのfavoriteプロパティをbool型からint型に変更 dbHelper追加 設定ファイルの更新とCFBundleLocalizationsの追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'theme.dart';
 // import 'package:flutter_markdown/flutter_markdown.dart';
 import 'dart:async';
-
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'dbHelper.dart';
 import 'package:uuid/uuid.dart';
