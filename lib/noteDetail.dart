6310b182561334db6f60e71c3ff1f6132872e006
powerninja <asdyuy23@gmail.com>
2024-05-14 00:45:55 +0900

    メモ編集画面の実装を追加

---
+++ lib/noteDetail.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+import 'dart:async';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'dbHelper.dart';
+import 'package:uuid/uuid.dart';
+
+class NoteDetail extends StatefulWidget {
+  final Note? note;
+
+  const NoteDetail({
+    Key? key,
+    this.note,
+  }) : super(key: key);
+
+  @override
+  _NoteDetailState createState() => _NoteDetailState();
+}
+
+class _NoteDetailState extends State<NoteDetail> {
+  late TextEditingController _titleController;
+  late TextEditingController _contentController;
+  late String _title;
+  late String _content;
+  late String _date;
+  late String _color;
+  late String _imagePath;
+  late int _favorite;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _title = widget.note!.title;
+    _content = widget.note!.content;
+    _date = widget.note!.date;
+    _color = widget.note!.color;
+    _imagePath = widget.note!.imagePath!;
+    _favorite = widget.note!.favorite;
+
+    _titleController = TextEditingController(text: _title);
+    _contentController = TextEditingController(text: _content);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('メモ'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () async {
+              final note = Note(
+                id: widget.note!.id,
+                title: _titleController.text,
+                content: _contentController.text,
+                date: _date,
+                favorite: _favorite,
+                color: _color,
+                imagePath: _imagePath,
+              );
+              await note.updateNote(note);
+              // Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            controller: _titleController,
+            decoration: const InputDecoration(
+              hintText: 'タイトル',
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            onChanged: (value) {
+              setState(() {
+                _title = value;
+              });
+            },
+          ),
+          Expanded(
+            child: TextField(
+              controller: _contentController,
+              decoration: const InputDecoration(
+                hintText: '内容',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(16),
+              ),
+              maxLines: null,
+              onChanged: (value) {
+                setState(() {
+                  _content = value;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // フローティングアクションボタン
+      floatingActionButton: Row(
+        // 中心に配置
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // 保存ボタン
+          FloatingActionButton.extended(
+            label: const Text('Save'),
+            icon: const Icon(Icons.save),
+            onPressed: () async {
+              final note = Note(
+                id: widget.note!.id,
+                title: _titleController.text,
+                content: _contentController.text,
+                date: _date,
+                favorite: _favorite,
+                color: _color,
+                imagePath: _imagePath,
+              );
+              await note.updateNote(note);
+
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
